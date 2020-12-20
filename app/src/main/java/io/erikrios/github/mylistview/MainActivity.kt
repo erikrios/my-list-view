bbe49021f24eb1c65ae08a96889cfe37aa066762
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-20 20:45:06 +0700

    Integrate the list view in activity

--- app/src/main/java/io/erikrios/github/mylistview/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/mylistview/MainActivity.kt
@@ -1,11 +1,22 @@
 package io.erikrios.github.mylistview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import io.erikrios.github.mylistview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val dataName = arrayOf("Cut Nyak Dien", "Ki Hajar Dewantara", "Moh Yamin", "Pattimura", "R A Kartini", "Sukarno")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, dataName)
+        binding.lvList.adapter = adapter
     }
 }
